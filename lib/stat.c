e6d91150552b2cad35729be1dcf8633e3b71220d
Mitch Capper <mitch.capper@gmail.com>
2023-04-22 21:29:25 -0700

    Simplify symlink file mode setting and don't check symlinked directories for being executable

--- lib/stat.c
+++ lib/stat.c
@@ -301,14 +301,17 @@ _rpl_stat (char const *name, struct stat *buf, int do_lstat)
       buf->st_ino = 0;
 # endif
 
+	  int is_sym_link = info.dwFileAttributes & FILE_ATTRIBUTE_REPARSE_POINT;
       /* st_mode.  */
       unsigned int mode =
         /* XXX How to handle FILE_ATTRIBUTE_REPARSE_POINT ?  */
-        ((info.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) ? _S_IFDIR | S_IEXEC_UGO : _S_IFREG)
-        | (info.dwFileAttributes & FILE_ATTRIBUTE_REPARSE_POINT ? S_IFLNK : 0)
+		  ((info.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) ? _S_IFDIR | S_IEXEC_UGO : 0)
         | S_IREAD_UGO
+		  | (is_sym_link ? S_IFLNK : 0)
         | ((info.dwFileAttributes & FILE_ATTRIBUTE_READONLY) ? 0 : S_IWRITE_UGO);
-      if (!(info.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
+	  if (!(info.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY || is_sym_link))
+		  mode |= _S_IFREG;
+	  if (!(info.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY && !is_sym_link))
         {
           /* Determine whether the file is executable by looking at the file
              name suffix.  */
