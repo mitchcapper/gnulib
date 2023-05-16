cb1c4b706c00b5e5c7cfb9dd4410bcc2e67fc0a0
Mitch Capper <mitch.capper@gmail.com>
2023-05-15 21:04:18 -0700

    Updated S_ISDIR to work for sym linked directories, and added S_ISHARDDIR to return only if it is an actual directory

--- lib/sys_stat.in.h
+++ lib/sys_stat.in.h
@@ -260,6 +260,9 @@ struct stat
 #undef S_IFMT
 #define S_IFMT (_S_IFMT | S_IFLNK)
 #define S_ISLNK(m) (((m) & S_IFMT) == S_IFLNK)
+#define S_ISHARDDIR(m) (((m) & S_IFMT) == S_IFDIR)
+#undef S_ISDIR
+#define S_ISDIR(m) (((m) & (S_IFMT ^ S_IFLNK)) == S_IFDIR)
 
 #endif
 
